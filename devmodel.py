d1d732f02f3383b2061e55fb343ed9b971adeefe
novalinzai <nova.linzai@gmail.com>
2025-04-24 06:16:56 +0700

    Update devmodel.py

--- devmodel.py
+++ devmodel.py
@@ -14,4 +14,4 @@ years_experience = st.number_input("Masukkan Masa Dinas:", min_value=0.0, step=0
 #Prediksi gaji
 if st.button("Prediksi Gaji"):
    gaji = lin_reg_loaded.predict([[years_experience]])
-   st.write(f"Gaji seseorang dengan masa dinas {years_experience} tahun adalah ${salary[0]:,.2f}")
+   st.write(f"Gaji seseorang dengan masa dinas {years_experience} tahun adalah ${gaji[0]:,.2f}")
